c5a09848602ac4621a442b7791a00f137c9dc82d
TimeWizz <diego.laboren.97@gmail.com>
2023-12-04 19:40:33 -0500

    Finished Puzzle

--- Day 1/Puzzle1.mjs
+++ Day 1/Puzzle1.mjs
@@ -1,4 +1,19 @@
 import { text } from "./Puzzle1Text.mjs"
 
-const newText = text.split();
-console.log(newText);
+const textArr = text.split("\n");
+const numRegEx = /\d+/g;
+
+const splitNums = (array) => {
+    let total = 0;
+    for (const sub of array) {
+        let str1 = sub.match(numRegEx).join("")
+        console.log(str1)
+        str1 = Number(str1[0] + str1[str1.length - 1])
+        console.log(str1)
+        total = total + str1;
+    }
+    
+    console.log(total)
+}
+
+splitNums(textArr);
